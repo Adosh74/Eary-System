a98443179007de654ad1059cc65b41fc1d8f800f
alihamada <115657411+alihamada2@users.noreply.github.com>
2023-04-18 17:12:31 +0200

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/Login';
+import { Login } from "./components/Login";
+import { Register } from "./components/Register";
+import {Loginasuser} from './components/Loginasuser';
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+
+  }
+  
   return (
-   <>
-   <Login />
-   </>
-    
+    <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} />  : <Loginasuser onFormSwitch={toggleForm}  />  
+        ||  <Register onFormSwitch={toggleForm} />
+      }
+    </div>
   );
 }
 
