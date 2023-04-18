b538c23529a18906983806f8187268873a498c2c
alihamada <115657411+alihamada2@users.noreply.github.com>
2023-04-18 12:49:01 +0200

    push

---
+++ client/src/components/Login.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div class="Login-box">
+      <h1>Admin</h1>
+      <form>
+        <label>Email</label>
+        <input type="email" placeholder="" />
+        <label>Password</label>
+        <input type="password" placeholder="" />
+        <button class='submit'>Login</button>
+        <closeform></closeform></form>
+        <p class="para-2">
+      Are you Normal User? <a href="">Log in as user</a>
+    </p>
+    </div>
+  )
+}
+
+export default Login
