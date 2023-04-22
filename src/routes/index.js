cb0f87b3de2590c48c92d2f8b1eb7e93b46e80e6
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-04-22 11:02:12 +0200

    add exam routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import userRoutes from './APIs/user.route.js';
 import questionRoutes from './APIs/question.route.js';
 import answerRoutes from './APIs/answer.route.js';
+import examRoutes from './APIs/exam.route.js';
 
 const routes = Router();
 
 routes.use('/user', userRoutes);
 routes.use('/question', questionRoutes);
 routes.use('/answer', answerRoutes);
+routes.use('/exam', examRoutes);
 
 export default routes;
