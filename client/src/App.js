5a032b9dbb3e95d1179b68c32b4fe4c540fcbeee
alihamada <115657411+alihamada2@users.noreply.github.com>
2023-04-23 19:48:01 +0200

    .

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,13 @@ import { Login } from "./components/Login";
 import { Register } from "./components/Register";
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
-
   const toggleForm = (formName) => {
     setCurrentForm(formName);
-
   }
-  
   return (
     <div className="App">
       {
         currentForm === "login" ? <Login onFormSwitch={toggleForm} />  : <Register onFormSwitch={toggleForm}  />  
-        ||  <Register onFormSwitch={toggleForm} />
       }
     </div>
   );
