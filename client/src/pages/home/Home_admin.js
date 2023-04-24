6f9a029ed3ebec0dc0f5eb5cf82b7114d5141dcf
alihamada <115657411+alihamada2@users.noreply.github.com>
2023-04-24 15:51:23 +0200

    .

--- client/src/pages/home/Home_admin.js
+++ client/src/pages/home/Home_admin.js
@@ -1,10 +1,27 @@
 import React from "react";
-
+import'./Home_admin.css'
+import { Link } from "react-router-dom";
 
 const Home_admin = () => {
     return (
-         <div>
-      
+         <div className="container">
+      <div className="intro">
+<h1 className="intro-title">Hello  Admin</h1>
+<p>
+  kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+</p>
+<p>
+  kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+</p>
+
+<Link to="#">
+<button className="btn">
+  Quiz
+</button>
+
+</Link>
+
+      </div>
          </div>
     );
 };
