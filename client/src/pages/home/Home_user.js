d7b0ac8f5ef02e8de825d9b6860eeff668c85fb6
Mohamed Shaban <mohamed.shabanzakaryia@gmail.com>
2023-04-24 15:02:15 +0200

    home

--- client/src/pages/home/Home_user.js
+++ client/src/pages/home/Home_user.js
@@ -4,7 +4,7 @@ import React from "react";
 const Home_user = () => {
     return (
          <div>
-        <img  className="p-0" src="https://picsum.photos/200/300" />
+        
          </div>
     );
 };
