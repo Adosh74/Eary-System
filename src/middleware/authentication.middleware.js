74a8a6ad1473de4de0fb85b6243336f151c5d4c8
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-04-24 16:37:59 +0200

    add admin middleware

--- src/middleware/authentication.middleware.js
+++ src/middleware/authentication.middleware.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 import config from './../../config/config.js';
 
+//** +[1] check if the user is authenticated **//
 export const isAuthenticated = (req, res, next) => {
   const token = req.header('x-auth-token');
   if (!token) return res.status(401).send('access denied');
@@ -9,6 +10,28 @@ export const isAuthenticated = (req, res, next) => {
     const decodedPayload = jwt.verify(token, config.tokenSecret);
 
     if (!decodedPayload) return res.status(400).send('invalid token');
+
+    next();
+  } catch (error) {
+    return res.status(400).send('Invalid token');
+  }
+};
+
+//** +[2] check if admin **//
+export const isAdmin = (req, res, next) => {
+  const token = req.header('x-auth-token');
+  if (!token) return res.status(401).send('access denied');
+
+  try {
+    const decodedPayload = jwt.verify(token, config.tokenSecret);
+
+    if (!decodedPayload) return res.status(400).send('invalid token');
+
+    if (!decodedPayload.isAdmin) {
+      return res
+        .status(401)
+        .send('you are unauthorized access this endpoint');
+    }
 
     next();
   } catch (error) {
