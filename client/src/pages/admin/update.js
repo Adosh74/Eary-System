98e34a79ab6beb5c4995a57d45168c277913d9bd
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-04-25 18:02:45 +0200

    Update update.js

--- client/src/pages/admin/update.js
+++ client/src/pages/admin/update.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from "react";
 import Button from 'react-bootstrap/Button';
 import { Link } from "react-router-dom";
@@ -10,3 +11,21 @@ import { Link } from "react-router-dom";
     );
  };
  export default Update;
+=======
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+
+const Update = () => {
+  return (
+    <div>
+      <Link to="/home_admin">
+        <Button variant="primary" type="submit" className="submit">
+          Home
+        </Button>
+      </Link>
+    </div>
+  );
+};
+export default Update;
+>>>>>>> Stashed changes
