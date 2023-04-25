fcd06bd6487a3785c8d7c5e21e290e1bc3b9860e
alihamada <115657411+alihamada2@users.noreply.github.com>
2023-04-25 16:27:17 +0200

    .

--- client/src/pages/admin/approve.js
+++ client/src/pages/admin/approve.js
@@ -1,12 +1,46 @@
 import React from "react";
-
+import './approve.css'
 
  const Approve = () => {
     return(
-        <div>
+        <div >
+<table >
 
-                 <h1>Approve</h1>
+ 
+
+<tr>
+
+<th>Id</th>
+
+<th>Name</th>
+
+<th>Email Address</th>
+
+<th>Action </th>
+</tr>
+ <tr>
+
+<td>1</td>
+
+<td>Jean</td>
+
+<td>leBon</td>
+
+<td>1368</td>
+
+</tr>
+
+
+</table>
+<form  class='bu'>
+<button class="submit" type="submit"> submit</button>
+<button class="reject" type="reject"> reject</button>
+</form>
+
+
         </div>
     );
  };
  export default Approve;
+
+ /**/
