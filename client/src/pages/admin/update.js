b4528a413e67eb0854dcd663d5da4d95fa188016
alihamada <115657411+alihamada2@users.noreply.github.com>
2023-04-25 17:50:03 +0200

    ..

--- client/src/pages/admin/update.js
+++ client/src/pages/admin/update.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import Button from 'react-bootstrap/Button';
 import { Link } from "react-router-dom";
@@ -6,24 +5,8 @@ import { Link } from "react-router-dom";
  const Update =()=>{
     return(
    <div>
-            <Link  to="/home_admin">
-        <Button variant="primary" type="submit"  className="submit">
-        Home
-        </Button>
-        </Link>  
          
         </div>
     );
  };
  export default Update;
-=======
-import React from 'react';
-const Update = () => {
-  return (
-    <div>
-      <h1>Update</h1>
-    </div>
-  );
-};
-export default Update;
->>>>>>> f1659c9b9a4235a32636bf043f00ee9b7dac72c0
