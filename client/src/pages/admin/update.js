f1659c9b9a4235a32636bf043f00ee9b7dac72c0
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-04-25 16:38:06 +0200

    Update update.js

--- client/src/pages/admin/update.js
+++ client/src/pages/admin/update.js
@@ -1,10 +1,9 @@
-import React from "react";
- const Update =()=>{
-    return(
-        <div>
-
-                <h1>Update</h1>
-        </div>
-    );
- };
- export default Update;
+import React from 'react';
+const Update = () => {
+  return (
+    <div>
+      <h1>Update</h1>
+    </div>
+  );
+};
+export default Update;
