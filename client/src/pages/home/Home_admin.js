9740d234451860f289e907a1bd09ea8302732271
alihamada <115657411+alihamada2@users.noreply.github.com>
2023-04-26 16:51:59 +0200

    Update Home_admin.js

--- client/src/pages/home/Home_admin.js
+++ client/src/pages/home/Home_admin.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './Home_admin.css';
 import { Link } from 'react-router-dom';
-
+import Nav from 'react-bootstrap/Nav';
+import Navbar from "react-bootstrap/Navbar";
 const Home_admin = () => {
   return (
     <div className="container">
+
       <div className="intro">
         <h1 className="intro-title">Hello Admin</h1>
+        <form action="code.php" method="POST" class="d-inline">
+          <Link to='login_admin'>
+          <button type="submit" name="delete_student" value="<?=$student['id'];?>" class="btn btn-danger btn-sm">LogOut</button>
+      </Link>
+        </form>
         <p>Here You can create and update Quiz</p>
         <Link to="/quiz" className="link-1">
           <button class="btn btn1">Quiz</button>
@@ -29,7 +36,7 @@ const Home_admin = () => {
 
         <p>Here you can see Users</p>
         <Link to="/users" className="link-3">
-        <button class="btn btn1">Users</button>
+          <button class="btn btn1">Users</button>
         </Link>
         <br></br>
         <br></br>
