103d962bd96a8828d1dfed741a2614582d2730df
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-05-02 05:30:17 +0300

    add upload files middleware

---
+++ src/middleware/upload.js
@@ -0,0 +1,18 @@
+import multer from 'multer';
+import path from 'path';
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix =
+      Date.now() + '-' + Math.round(Math.random() * 1e9);
+    const extension = path.extname(file.originalname);
+    cb(null, file.fieldname + '-' + uniqueSuffix + extension);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+export default upload;
