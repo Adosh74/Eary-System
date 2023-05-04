8f860ea72ee15b85c31ce80d9c71824113a3cc87
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-05-05 02:56:31 +0300

    123

--- client/src/pages/admin/quiz/quiz.js
+++ client/src/pages/admin/quiz/quiz.js
@@ -1,67 +1,61 @@
-import React from "react";
-import img1 from './OIP.jpg'
-import img2 from './edit.jpg'
-import img3 from './sevenup.jpg'
-import './Quiz.css'
-import { Link } from 'react-router-dom';
+import React from 'react';
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
+import img1 from './OIP.jpg';
+import './Quiz.css';
+import img2 from './edit.jpg';
+import img3 from './sevenup.jpg';
 
- const Quiz =()=>{
-    return(
-        <div >
-        <div className="head" style={{paddingBottom:"0px",paddingRight:"1150px"}}>
-        <Link  to="/home_admin">
-        <Button variant="primary" type="submit"  style={{backgroundColor:"black"}} >
-        Home
-        </Button>
-        </Link>  
-        </div>
-        <div class="containe">
-           
+const Quiz = () => {
+  return (
+    <div>
+      <div className="containe">
+        <div className="card">
+          <Link to="/createquiz">
+            <button>
+              <input
+                type="Image"
+                src={img1}
+                alt="quiz"
+                className="quizimg"
+              ></input>
+            </button>
+          </Link>
 
-            <div class="card">
-
-                <Link to="/createquiz"> 
-          <button>
-                   <input type="Image" src={img1} alt="quiz" class="quizimg"></input> 
-                   
-                   </button> 
-                   
-                   </Link>
-
-
-                   
-                   <div class="introo">
-                    <h1 class="g">Quiz</h1>
-                    <p class="ll">The <span>Quiz</span> You can create it from Here </p>
-                   </div>
-                    </div>
-
-                    <div>      
-            < img src={img3} alt="logo" class="logo"></img> 
-            </div>
-
-                    <div class="card">
-                   
-                <Link to ="/editquiz"> 
-          <button>
-                   <input type="Image" src={img2} alt="quiz" class="edit"></input> 
-                   
-                   </button> 
-                   
-                   </Link>
-         
-         
-          <div class="introo"> 
-           <h1 class="g">Edit-Quiz</h1>
-           <p class="ll">The <span>Quiz</span> You can Edit it from Here </p>
+          <div className="introo">
+            <h1 className="g">Quiz</h1>
+            <p className="ll">
+              The <span>Quiz</span> You can create it from Here{' '}
+            </p>
           </div>
-           </div>
+        </div>
 
+        <div>
+          <img src={img3} alt="logo" className="logo"></img>
         </div>
-           
+
+        <div className="card">
+          <Link to="/editquiz">
+            <button>
+              <input
+                type="Image"
+                src={img2}
+                alt="quiz"
+                className="edit"
+              ></input>
+            </button>
+          </Link>
+
+          <div className="introo">
+            <h1 className="g">Edit-Quiz</h1>
+            <p className="ll">
+              The <span>Quiz</span> You can Edit it from Here{' '}
+            </p>
+          </div>
         </div>
-    );
- };
- export default Quiz;
+      </div>
+    </div>
+  );
+};
+export default Quiz;
