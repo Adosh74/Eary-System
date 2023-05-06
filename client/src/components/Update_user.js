73fa05cea9dd69e6c00437575da2d9e9d9ff753d
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-05-06 03:11:13 +0300

    add update user profile

--- client/src/components/Update_user.js
+++ client/src/components/Update_user.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useRef } from 'react';
 import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import '../pages/admin/update.css';
 import '../pages/authentication/Login.css';
+import { getAuthToken } from '../services/auth.service';
 
 const Update_user = () => {
+  const { token, user } = getAuthToken();
+  const navigate = useNavigate();
+
+  const form = useRef({
+    name: '',
+    email: '',
+    password: '',
+    phone: '',
+  });
+
+  const submit = e => {
+    e.preventDefault();
+    axios
+      .put(
+        `http://localhost:3000/user/${user.user_id}`,
+        {
+          name: form.current.name.value,
+          email: form.current.email.value,
+          password: form.current.password.value,
+          phone: form.current.phone.value,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then(data => {
+        alert(data.data.message);
+        navigate('/home_user');
+      })
+      .catch(err => {
+        alert(`something went wrong`);
+      });
+  };
+
   return (
     <div>
       <div
@@ -18,26 +56,38 @@ const Update_user = () => {
             className="form-container"
             style={{ backgroundColor: '#e4d3e6ca' }}
           >
-            <form action="" method="post">
+            <form onSubmit={e => submit(e)}>
               <h3>Update Profile</h3>
               <input
+                ref={val => {
+                  form.current.name = val;
+                }}
                 type="text"
                 name="name"
                 placeholder="Enter Your Name"
               ></input>
               <input
+                ref={val => {
+                  form.current.email = val;
+                }}
                 type="email"
                 name="email"
                 required
                 placeholder="enter your email"
               ></input>
               <input
+                ref={val => {
+                  form.current.password = val;
+                }}
                 type="password"
                 name="password"
                 required
                 placeholder="enter your password"
               ></input>
               <input
+                ref={val => {
+                  form.current.phone = val;
+                }}
                 type="text"
                 name="phone"
                 placeholder="Enter Your Phone Number"
